d9b8a704e6949cdfd7ae52c9afba67efe4a12f3d
MikeElnathan <mikeson.anthony@gmail.com>
2025-05-21 16:06:47 +0800

    Camera
    Handle input

--- Player/CameraArm.cs
+++ Player/CameraArm.cs
@@ -1,6 +1,33 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class CameraArm : Node3D
 {
+    private float orbit_speed;
+    private float orbit_angle;
+
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            if (Input.IsActionPressed("Orbit_Camera"))
+            {
+                GD.Print("Mouse moved: ", mouseMotion.Relative);
+            }
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+
+
 }
