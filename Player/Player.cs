d228a0684b22bc3baab613379a33a2660ec55780
MikeElnathan <mikeson.anthony@gmail.com>
2025-05-21 15:54:43 +0800

    Player, Camera
    Apply gravity, Set up camera.

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,4 +3,33 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    private float gravity;
+    private Vector3 _velocity;
+
+    public override void _Ready()
+    {
+        //AsSingle() convert the result to float 
+        gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+        GD.Print("Player Script: Ready entered");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        _velocity = Velocity;
+
+        if (!IsOnFloor())
+        {
+            _velocity.Y -= gravity;
+        }
+
+        Velocity = _velocity;
+        MoveAndSlide();
+    }
+
 }
