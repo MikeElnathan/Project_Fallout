9fea8733fb35572e0b79ac5414ddd4ea9adab377
MikeElnathan <mikeson.anthony@gmail.com>
2025-05-26 13:24:28 +0800

    Camera
    smooth zoom and orbit

--- Player/CameraArm.cs
+++ Player/CameraArm.cs
@@ -10,10 +10,11 @@ public partial class CameraArm : Node3D
     private float current_yaw = 0.0f;
     private float deadzone = 0.5f;
 
-    private float zoom = 0.0f;
+    private float zoom = 45.0f;
     private float zoom_value = 2.0f;
     private float zoom_min = 30.0f;
     private float zoom_max = 75.0f;
+    private float target_zoom;
 
     private Camera3D camera;
 
@@ -21,6 +22,7 @@ public partial class CameraArm : Node3D
     {
         current_yaw = RotationDegrees.Y;
         camera = GetChild<Camera3D>(0);
+        camera.Fov = zoom;
     }
 
     public override void _Input(InputEvent @event)
@@ -48,16 +50,21 @@ public partial class CameraArm : Node3D
                 GD.Print("wheel down");
             }
             zoom = Mathf.Clamp(zoom, zoom_min, zoom_max);
-            camera.Fov = Mathf.Lerp(camera.Fov, zoom, 1.0f);
         }
     }
 
     public override void _PhysicsProcess(double delta)
     {
         // orbit camera
-        current_yaw = orbit_velocity * (float)delta;
+        current_yaw += orbit_velocity * (float)delta;
 
         RotationDegrees = new Godot.Vector3(RotationDegrees.X, current_yaw, RotationDegrees.Z);
+
+        orbit_velocity *= 0.55f;
+        //
+        // Zoom camera
+        target_zoom = Mathf.Lerp(zoom, target_zoom, 0.85f);
+        camera.Fov = target_zoom;
         //
     }
 
