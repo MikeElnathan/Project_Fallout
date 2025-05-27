9aef30cbf86f76a78c3fe8e946c49e163b0511bd
MikeElnathan <mikeson.anthony@gmail.com>
2025-05-27 15:42:24 +0800

    Player
    Add sprint speed.
    Implementing state machine

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,14 +3,15 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-    private float speed = 5.0f;
+    private float speed;
+    private float run_speed = 10.0f;
+    private float walk_speed = 6.0f;
     private Godot.Vector3 move_dir = Godot.Vector3.Zero;
     private Godot.Vector2 input_dir = Godot.Vector2.Zero;
     private Godot.Vector3 target_velocity = Godot.Vector3.Zero;
     private float rotation_speed = 8.0f;
     private float move_damping = 25.0f;
 
-    private Godot.Vector3 _velocity;
     private float jump_gravity;
     private float jump_velocity;
     private float fall_gravity;
@@ -29,6 +30,9 @@ public partial class Player : CharacterBody3D
         camera = GetNode<Camera3D>("CameraAndMesh/Camera_Arm/Camera3D");
 
         visual_mesh = GetNode<Node3D>("CameraAndMesh/Mesh");
+
+        speed = walk_speed;
+
         Jump_Physics();
     }
 
@@ -38,17 +42,44 @@ public partial class Player : CharacterBody3D
         jump_gravity = (-2.0f * jump_height) / (float)Math.Pow(time_to_peak, 2);
         fall_gravity = (-2.0f * jump_height) / (float)Math.Pow(time_to_fall, 2);
     }
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventKey keyEvent)
+        {
+            if (keyEvent.Pressed && Input.IsActionJustPressed("Run"))
+            {
+                speed = run_speed;
+                GD.Print("run_speed");
+            }
+            else if (!keyEvent.Pressed && Input.IsActionJustReleased("Run"))
+            {
+                speed = walk_speed;
+                GD.Print("walk_speed");
+            }
 
-    private float Return_gravity()
+        }
+    }
+
+    private float return_gravity()
     {
         return Velocity.Y > 0.0f ? jump_gravity : fall_gravity;
     }
 
-    private void Jump()
+    private void jump()
     {
-        _velocity.Y = jump_velocity;
+        Velocity = new Godot.Vector3(Velocity.X, jump_velocity, Velocity.Z);
     }
+    private void rotate_Visual_Mesh()
+    {
+        if (move_dir.Length() > 0.1f)
+        {
+            float target_angle = Mathf.Atan2(move_dir.X, move_dir.Z);
+            float current_yaw = visual_mesh.Rotation.Y;
+            float smoothed_angle = Mathf.LerpAngle(current_yaw, target_angle, rotation_speed * (float)GetPhysicsProcessDeltaTime());
 
+            visual_mesh.Rotation = new Godot.Vector3(0, smoothed_angle, 0);
+        }
+    }
     private void Keyboard_move()
     {
         input_dir = Input.GetVector("Left", "Right", "Forward", "Backward");
@@ -65,27 +96,13 @@ public partial class Player : CharacterBody3D
         {
             move_dir = (right * input_dir.X + forward * -input_dir.Y).Normalized();
             target_velocity = move_dir * speed;
-            Rotate_Visual_Mesh();
+            rotate_Visual_Mesh();
         }
         else
         {
             target_velocity = Godot.Vector3.Zero;
         }
-
     }
-
-    private void Rotate_Visual_Mesh()
-    {
-        if (move_dir.Length() > 0.1f)
-        {
-            float target_angle = Mathf.Atan2(move_dir.X, move_dir.Z);
-            float current_yaw = visual_mesh.Rotation.Y;
-            float smoothed_angle = Mathf.LerpAngle(current_yaw, target_angle, rotation_speed * (float)GetPhysicsProcessDeltaTime());
-
-            visual_mesh.Rotation = new Godot.Vector3(0, smoothed_angle, 0);
-        }
-    }
-
     private void Handle_Movement(double delta)
     {
         Godot.Vector3 horizontal_velocity = new Godot.Vector3(Velocity.X, 0, Velocity.Z);
@@ -101,32 +118,27 @@ public partial class Player : CharacterBody3D
 
         Velocity = new Godot.Vector3(horizontal_velocity.X, Velocity.Y, horizontal_velocity.Z);
     }
-
     private void Handle_jump(double delta)
     {
-        _velocity.Y += Return_gravity() * (float)delta;
+        Velocity = new Godot.Vector3(Velocity.X, Velocity.Y + return_gravity() * (float)delta, Velocity.Z);
 
         if (Input.IsActionJustPressed("Jump") && IsOnFloor())
         {
-            Jump();
-            Godot.Vector3 jump_momentum = move_dir * speed * 0.3f;
-            target_velocity += jump_momentum;
+            jump();
+            Godot.Vector3 jump_momentum = move_dir * speed * 0.1f;
+            Velocity = new Godot.Vector3(Velocity.X + jump_momentum.X, Velocity.Y, Velocity.Z + jump_momentum.Z);
         }
 
-        if (IsOnFloor() && _velocity.Y < 0.0f)
+        if (IsOnFloor() && Velocity.Y < 0.0f)
         {
-            _velocity.Y = 0.0f;
+            Velocity = new Godot.Vector3(Velocity.X, 0.0f, Velocity.Z);
         }
-
-        Velocity = new Godot.Vector3(Velocity.X, _velocity.Y, Velocity.Z);
     }
-
     public override void _PhysicsProcess(double delta)
     {
         Keyboard_move();
         Handle_Movement(delta);
         Handle_jump(delta);
-
 
         MoveAndSlide();
     }
