a5458737cfb151430b8c93eb25631b8631f457bc
MikeElnathan <mikeson.anthony@gmail.com>
2025-05-28 15:58:12 +0800

    Statemachine
    Implement statemachine template

--- Player/StateMachine.cs
+++ Player/StateMachine.cs
@@ -1,6 +1,36 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class StateMachine : Node3D
 {
+    private State currentState;
+    private Dictionary<string, State> states = new();
+
+    public void addState(string name, State state)
+    {
+        if (!states.ContainsKey(name))
+        {
+            states.Add(name, state);
+        }
+    }
+    public void changeState(string name)
+    {
+        if (!states.ContainsKey(name)) { return; }
+
+        currentState?.Exit();
+        currentState = states[name];
+        currentState.Enter();
+    }
+
+    public override void _Process(double delta)
+    {
+        currentState?.Update(delta);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        currentState.PhysicUpdate(delta);
+    }
+
 }
