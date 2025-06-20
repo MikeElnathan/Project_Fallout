8129ecd29e8f0174f27dfa9543d8011279d583b0
MikeElnathan <mikeson.anthony@gmail.com>
2025-06-20 15:01:02 +0800

    Update Noel.CS
    Rudimentary follow player. Can't detect collision.

--- Kid/Noel.cs
+++ Kid/Noel.cs
@@ -5,20 +5,41 @@ public partial class Noel : CharacterBody3D
 {
     private CharacterBody3D Player;
     private Vector3 PlayerPosition;
+    private float DistanceToPlayer;
+    private float MaxDistance;
+    private float Speed;
 
     public override void _Ready()
     {
         Player = GetTree().GetFirstNodeInGroup("Player") as CharacterBody3D;
+        //Temporary value assignment. Later, to be manipulated by behavior tree
+        MaxDistance = 5.0f;
+        Speed = 100.0f;
+    }
+    private void FollowPlayer(double _delta)
+    {
+        if (DistanceToPlayer > MaxDistance)
+        {
+            Vector3 Direction = (PlayerPosition - GlobalPosition).Normalized();
+            Velocity = Direction * Speed * (float)_delta;
+        }
+        else
+        {
+            Velocity = Vector3.Zero;
+        }
     }
     public override void _Process(double delta)
     {
         //TODO
-        PlayerPosition = Player.GlobalPosition;
-        GD.Print(PlayerPosition);
     }
-
     public override void _PhysicsProcess(double delta)
     {
+        PlayerPosition = Player.GlobalPosition;
+        DistanceToPlayer = GlobalPosition.DistanceTo(PlayerPosition);
+        GD.Print("Distance to player: ", DistanceToPlayer);
+
+        FollowPlayer(delta);
+        MoveAndSlide();
     }
 
 }
