26f1f42ea8e21d8e36adb30bb81a42f6ba4c2910
MikeElnathan <mikeson.anthony@gmail.com>
2025-07-04 11:08:02 +0800

    Create a branch
    experiment with Noel state machine

--- Kid/Noel.cs
+++ Kid/Noel.cs
@@ -3,35 +3,9 @@ using System;
 
 public partial class Noel : CharacterBody3D
 {
-    private CharacterBody3D Player;
-    private Vector3 PlayerPosition;
-    private float DistanceToPlayer;
-    private float MaxDistance;
-    private float Speed;
-    private bool followPlayer;
-
     public override void _Ready()
     {
-        Player = GetTree().GetFirstNodeInGroup("Player") as CharacterBody3D;
-        //Temporary value assignment. Later, to be manipulated by behavior tree
-        MaxDistance = 3.0f;
-        Speed = 200.0f;
-    }
-    private void CheckDistance()
-    {
-        if (DistanceToPlayer > MaxDistance)
-        {
-            followPlayer = true;
-        }
-        if (DistanceToPlayer < 2.0f)
-        {
-            followPlayer = false;
-        }
-    }
-    private void FollowPlayer(double _delta)
-    {
-        Vector3 Direction = (PlayerPosition - GlobalPosition).Normalized();
-        Velocity = Direction * Speed * (float)_delta;
+        base._Ready();
     }
     public override void _Process(double delta)
     {
@@ -39,20 +13,6 @@ public partial class Noel : CharacterBody3D
     }
     public override void _PhysicsProcess(double delta)
     {
-        PlayerPosition = Player.GlobalPosition;
-        DistanceToPlayer = GlobalPosition.DistanceTo(PlayerPosition);
-        CheckDistance();
-
-        if (followPlayer)
-        {
-            FollowPlayer(delta);
-        }
-        else
-        {
-            Velocity = Vector3.Zero;
-        }
-
-        MoveAndSlide();
+        
     }
-
 }
