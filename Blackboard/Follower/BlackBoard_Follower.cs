caebd40c905534ed84644e024bd7914e3c684c1a
MikeElnathan <mikeson.anthony@gmail.com>
2026-09-11 16:09:21 +0800

    cleanup noel to follower

--- Blackboard/Follower/BlackBoard_Follower.cs
+++ Blackboard/Follower/BlackBoard_Follower.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class NoelIdle : State
+public partial class BlackBoard_Follower : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
--- Kid/NPC_Follower_Template/FollowerStateMachine.cs
+++ Kid/NPC_Follower_Template/FollowerStateMachine.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class NoelRun : State
+public partial class FollowerStateMachine : BaseStateMachine
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
